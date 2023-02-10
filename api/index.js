d5760fe9746973face6677e09b464640613a9728
DESKTOP-SFEKABV\Coral Miller <coral1608@gmail.com>
2023-02-09 21:48:03 -0800

    get random kachow endpoint

--- api/index.js
+++ api/index.js
@@ -19,6 +19,25 @@ app.get('/movies', function (reqest, response) {
   });
 });
 
+app.get('/ka-chows/random', function (request, response) {
+  const include = (request.query.include || '').split(',');
+  const randomIndex = Math.floor(Math.random() * data.kaChows.length);
+  const randomKaChow = data.kaChows[randomIndex];
+  if (include.includes('movie')) {
+    const matchingMovie = data.movies.find(function (movie) {
+      return movie.id === randomKaChow.movieId;
+    });
+    randomKaChow.movie = matchingMovie;
+  }
+  if (include.includes('character')) {
+    const matchingCharacter = data.characters.find(function (character) {
+      return character.id === randomKaChow.characterId;
+    });
+    randomKaChow.charater = matchingCharacter;
+  }
+  response.send(randomKaChow);
+});
+
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
