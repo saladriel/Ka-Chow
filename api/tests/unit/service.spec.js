0fd948d41ee8225902696518538d13c77b61cb1a
Farren Casad <fcasad@gmail.com>
2023-02-15 21:40:32 -0800

    Add randomness test

--- api/tests/unit/service.spec.js
+++ api/tests/unit/service.spec.js
@@ -1,6 +1,29 @@
 const service = require('../../service');
 
 describe('getRandomKaChow', function () {
+  it('should be sufficiently random', function () {
+    /**
+     * Chi-Square Goodness of Fit produced values consistently over critical value, so using a more
+     * relaxed test to check the uniformity of distribution.
+     */
+    const sampleSize = 230000; // This is arbitrary but should be large enough to prevent flakiness.
+    const kaChowCount = 23;
+    const expectedFrequency = sampleSize / kaChowCount;
+    const variance = expectedFrequency * 0.1;
+    const distribution = {};
+    for (let i = 0; i < sampleSize; i++) {
+      const kaChow = service.getRandomKaChow([]);
+      distribution[kaChow.id] = (distribution[kaChow.id] || 0) + 1;
+    }
+
+    expect(Object.keys(distribution).length).toBe(kaChowCount);
+    Object.values(distribution).forEach((actualFrequency) => {
+      expect(Math.abs(actualFrequency - expectedFrequency)).toBeLessThanOrEqual(
+        variance
+      );
+    });
+  });
+
   it('should return a Ka-Chow with nothing included', function () {
     const actual = service.getRandomKaChow([]);
     const expected = {
