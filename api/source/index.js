dfafa211bdcb0aba1e1620045f2c2d663123f494
Farren Casad <fcasad@gmail.com>
2023-05-21 23:17:07 -0700

    Formatting

--- api/source/index.js
+++ api/source/index.js
@@ -1,6 +1,6 @@
-const app = require("./app");
+const app = require('./app');
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Ka-Chow API listening on port: ${port}`);
